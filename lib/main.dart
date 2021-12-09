42e940455f888d2ae67d9ef897fd13424fd0919f
pplotko <pavel.plotko@gmail.com>
2021-12-09 17:25:45 +0300

    Add search field in stack

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:json_placeholder_app/widgets/main_screen/main_screen_widget.dart';
 import 'Theme/app_colors.dart';
-import 'pages/home_page.dart';
-import 'pages/pony_list_page.dart';
+// import 'pages/home_page.dart';
+// import 'pages/pony_list_page.dart';
 import 'package:json_placeholder_app/widgets/auth/auth_widget.dart';
 void main() => runApp(MyApp());
 
--- lib/widgets/recipe_list/recipe_list_widget.dart
+++ lib/widgets/recipe_list/recipe_list_widget.dart
@@ -3,80 +3,165 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:json_placeholder_app/resources/resources.dart';
 
+class Recipe {
+  final int id;
+  final String imageName;
+  final String title;
+  final String time;
+  final String description;
+
+  Recipe({
+    required this.id,
+    required this.imageName,
+    required this.title,
+    required this.time,
+    required this.description,
+  });
+}
+
+// создаем список пони
+// final указывает на то, что мы больше
+// никогда не сможем присвоить имени _recipe
+// другой список рецептов
+final _recipe = [
+  Recipe(
+    id: 0,
+    imageName: AppImages.kotletiKlassicheskie2480300x233,
+    title: 'КОТЛЕТЫ КЛАССИЧЕСКИЕ (СВИНИНА + КУРИЦА)',
+    time: 'December 17, 2021',
+    description: 'Готовим фарш, перекручиваем мясо (свинину и курицу) на тонкой сетке мясорубки 2 раза. Лук сразу мелко мелко шинкуем. Обжариваем (если планируем заморозку). Добавляем в фарш. Хлебный мякиш отправляем в кипяток размочиться. Выдавливаем чеснок, солим и перчим по вкусу. Добавляем в мясо хлеб, пропустив через мясорубку и тщательно все перемешиваем. Сейчас руками сформируем мясные шарики и немного приплюснем. Руки при этом мочим в холодной воде, чтобы фарш не прилипал к ним. Теперь обваляем котлеты в муке или можно использовать любую другую панировку. Сначала одну сторону обжариваем на среднем огне, потом убавляем огонь до маленького, переворачиваем котлетки и накрываем крышкой. Добавляем к котлетам воды и тушим. После доводим до готовности и готово. Котлетки готовы. Приятного аппетита!',
+  ),
+  Recipe(
+    id: 1,
+    imageName: AppImages.kotletiKlassicheskie2480300x233,
+    title: 'ПЛОВ',
+    time: 'December 17, 2021',
+    description: 'Готовим фарш, перекручиваем мясо (свинину и курицу) на тонкой сетке мясорубки 2 раза. Лук сразу мелко мелко шинкуем. Обжариваем (если планируем заморозку). Добавляем в фарш. Хлебный мякиш отправляем в кипяток размочиться. Выдавливаем чеснок, солим и перчим по вкусу. Добавляем в мясо хлеб, пропустив через мясорубку и тщательно все перемешиваем. Сейчас руками сформируем мясные шарики и немного приплюснем. Руки при этом мочим в холодной воде, чтобы фарш не прилипал к ним. Теперь обваляем котлеты в муке или можно использовать любую другую панировку. Сначала одну сторону обжариваем на среднем огне, потом убавляем огонь до маленького, переворачиваем котлетки и накрываем крышкой. Добавляем к котлетам воды и тушим. После доводим до готовности и готово. Котлетки готовы. Приятного аппетита!',
+  ),
+  Recipe(
+    id: 2,
+    imageName: AppImages.kotletiKlassicheskie2480300x233,
+    title: 'КОТЛЕТЫ КЛАССИЧЕСКИЕ (СВИНИНА + КУРИЦА)',
+    time: 'December 17, 2021',
+    description: 'Готовим фарш, перекручиваем мясо (свинину и курицу) на тонкой сетке мясорубки 2 раза. Лук сразу мелко мелко шинкуем. Обжариваем (если планируем заморозку). Добавляем в фарш. Хлебный мякиш отправляем в кипяток размочиться. Выдавливаем чеснок, солим и перчим по вкусу. Добавляем в мясо хлеб, пропустив через мясорубку и тщательно все перемешиваем. Сейчас руками сформируем мясные шарики и немного приплюснем. Руки при этом мочим в холодной воде, чтобы фарш не прилипал к ним. Теперь обваляем котлеты в муке или можно использовать любую другую панировку. Сначала одну сторону обжариваем на среднем огне, потом убавляем огонь до маленького, переворачиваем котлетки и накрываем крышкой. Добавляем к котлетам воды и тушим. После доводим до готовности и готово. Котлетки готовы. Приятного аппетита!',
+  ),
+  Recipe(
+    id: 3,
+    imageName: AppImages.kotletiKlassicheskie2480300x233,
+    title: 'КОТЛЕТЫ КЛАССИЧЕСКИЕ (СВИНИНА + КУРИЦА)',
+    time: 'December 17, 2021',
+    description: 'Готовим фарш, перекручиваем мясо (свинину и курицу) на тонкой сетке мясорубки 2 раза. Лук сразу мелко мелко шинкуем. Обжариваем (если планируем заморозку). Добавляем в фарш. Хлебный мякиш отправляем в кипяток размочиться. Выдавливаем чеснок, солим и перчим по вкусу. Добавляем в мясо хлеб, пропустив через мясорубку и тщательно все перемешиваем. Сейчас руками сформируем мясные шарики и немного приплюснем. Руки при этом мочим в холодной воде, чтобы фарш не прилипал к ним. Теперь обваляем котлеты в муке или можно использовать любую другую панировку. Сначала одну сторону обжариваем на среднем огне, потом убавляем огонь до маленького, переворачиваем котлетки и накрываем крышкой. Добавляем к котлетам воды и тушим. После доводим до готовности и готово. Котлетки готовы. Приятного аппетита!',
+  ),
+  Recipe(
+    id: 4,
+    imageName: AppImages.kotletiKlassicheskie2480300x233,
+    title: 'КОТЛЕТЫ КЛАССИЧЕСКИЕ (СВИНИНА + КУРИЦА)',
+    time: 'December 17, 2021',
+    description: 'Готовим фарш, перекручиваем мясо (свинину и курицу) на тонкой сетке мясорубки 2 раза. Лук сразу мелко мелко шинкуем. Обжариваем (если планируем заморозку). Добавляем в фарш. Хлебный мякиш отправляем в кипяток размочиться. Выдавливаем чеснок, солим и перчим по вкусу. Добавляем в мясо хлеб, пропустив через мясорубку и тщательно все перемешиваем. Сейчас руками сформируем мясные шарики и немного приплюснем. Руки при этом мочим в холодной воде, чтобы фарш не прилипал к ним. Теперь обваляем котлеты в муке или можно использовать любую другую панировку. Сначала одну сторону обжариваем на среднем огне, потом убавляем огонь до маленького, переворачиваем котлетки и накрываем крышкой. Добавляем к котлетам воды и тушим. После доводим до готовности и готово. Котлетки готовы. Приятного аппетита!',
+  ),
+  Recipe(
+    id: 5,
+    imageName: AppImages.kotletiKlassicheskie2480300x233,
+    title: 'КОТЛЕТЫ КЛАССИЧЕСКИЕ (СВИНИНА + КУРИЦА)',
+    time: 'December 17, 2021',
+    description: 'Готовим фарш, перекручиваем мясо (свинину и курицу) на тонкой сетке мясорубки 2 раза. Лук сразу мелко мелко шинкуем. Обжариваем (если планируем заморозку). Добавляем в фарш. Хлебный мякиш отправляем в кипяток размочиться. Выдавливаем чеснок, солим и перчим по вкусу. Добавляем в мясо хлеб, пропустив через мясорубку и тщательно все перемешиваем. Сейчас руками сформируем мясные шарики и немного приплюснем. Руки при этом мочим в холодной воде, чтобы фарш не прилипал к ним. Теперь обваляем котлеты в муке или можно использовать любую другую панировку. Сначала одну сторону обжариваем на среднем огне, потом убавляем огонь до маленького, переворачиваем котлетки и накрываем крышкой. Добавляем к котлетам воды и тушим. После доводим до готовности и готово. Котлетки готовы. Приятного аппетита!',
+  ),
+];
+
+
+
 class RecipeListWidget extends StatelessWidget {
   const RecipeListWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     // return Container(color: Colors.green);
-  final img = AssetImage(AppImages.kotletiKlassicheskie2480300x233);
-    return ListView.builder(
-      itemCount: 10,
-      itemExtent: 163,
-      itemBuilder: (BuildContext context, int index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16),
-          child: Stack(
-            children: [DecoratedBox(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.black.withOpacity(0.2)),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                boxShadow:[
-                  BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      blurRadius: 8,
-                      offset: Offset(0,4),
-                  ),],
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                  child: Row(
-                    // color: Colors.green,
-                    children: [Image(image: img),
-                    SizedBox(width: 15),
-                    Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(height: 20),
-                            Text('КОТЛЕТЫ КЛАССИЧЕСКИЕ (СВИНИНА + КУРИЦА)',
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold,),
+  // final img = AssetImage(AppImages.kotletiKlassicheskie2480300x233);
+    return Stack(
+      children: [
+        ListView.builder(
+          padding: EdgeInsets.only(top: 70),
+          itemCount: _recipe.length,
+          itemExtent: 163,
+          itemBuilder: (BuildContext context, int index) {
+            final recipe = _recipe[index];
+            return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16),
+                  child: Stack(
+                    children: [DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.black.withOpacity(0.2)),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        boxShadow:[
+                          BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              blurRadius: 8,
+                              offset: Offset(0,4),
+                          ),],
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                          child: Row(
+                            // color: Colors.green,
+                            children: [Image(image: AssetImage(recipe.imageName)),
+                            SizedBox(width: 15),
+                            Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 20),
+                                    Text(recipe.title,
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold,),
+                                    ),
+                                    SizedBox(height: 5),
+                                    Text(recipe.time,
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(color: Colors.grey,),
+                                    ),
+                                    SizedBox(height: 20),
+                                    Text(recipe.description,
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                ),
                             ),
-                            SizedBox(height: 5),
-                            Text('17.12.2021',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(color: Colors.grey,),
-                            ),
-                            SizedBox(height: 20),
-                            Text('Готовим фарш, перекручиваем мясо (свинину и курицу) на тонкой сетке мясорубки 2 раза. Лук сразу мелко мелко шинкуем. Обжариваем (если планируем заморозку). Добавляем в фарш. Хлебный мякиш отправляем в кипяток размочиться. Выдавливаем чеснок, солим и перчим по вкусу. Добавляем в мясо хлеб, пропустив через мясорубку и тщательно все перемешиваем. Сейчас руками сформируем мясные шарики и немного приплюснем. Руки при этом мочим в холодной воде, чтобы фарш не прилипал к ним. Теперь обваляем котлеты в муке или можно использовать любую другую панировку. Сначала одну сторону обжариваем на среднем огне, потом убавляем огонь до маленького, переворачиваем котлетки и накрываем крышкой. Добавляем к котлетам воды и тушим. После доводим до готовности и готово. Котлетки готовы. Приятного аппетита!',
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ],
+                            SizedBox(width: 10),
+                            ]
+                          ),
                         ),
-                    ),
-                    SizedBox(width: 10),
-                    ]
+                      ),
+                        Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(10),
+                            onTap: () {
+                              print('Click');
+                            },
+                          ),
+                        ),
+                  ],
                   ),
-                ),
-              ),
-                Material(
-                  color: Colors.transparent,
-                  child: InkWell(
-                    borderRadius: BorderRadius.circular(10),
-                    onTap: () {
-                      print('Click');
-                    },
-                  ),
-                ),
-          ],
+                );
+          },
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white.withAlpha(235),
+              border: OutlineInputBorder(),
+              labelText: 'Поиск',
+            ),
           ),
-        );
-      },
+        ),
+        // SizedBox(height: 10,)
+      ],
     );
   }
 }
