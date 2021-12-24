ebfdc75b5569724b9480cf5da45f880ab45adab0
pplotko <pavel.plotko@gmail.com>
2021-12-24 15:54:25 +0300

    Add dayList actions

--- lib/widgets/days_list/days_list_widget.dart
+++ lib/widgets/days_list/days_list_widget.dart
@@ -2,8 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:json_placeholder_app/widgets/days_list/week_days.dart';
 import 'package:json_placeholder_app/Theme/app_colors.dart';
 
-class DaysListWidget extends StatelessWidget {
-  const DaysListWidget({Key? key}) : super(key: key);
+class DaysListWidget extends StatefulWidget {
+  DaysListWidget({Key? key}) : super(key: key);
+
+  @override
+  State<DaysListWidget> createState() => _DaysListWidgetState();
+}
+
+class _DaysListWidgetState extends State<DaysListWidget> {
+  var dayListState = List.filled (7, 0);
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,20 +24,52 @@ class DaysListWidget extends StatelessWidget {
         itemBuilder: (BuildContext context, int index) {
           return GestureDetector(
             onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return Text('Detail page');
-              },),
-              );
+              dayListState [index] == 0 ? dayListState [index] = 1 : dayListState [index] = 0;
+              print(index);
+              setState(() {
+              });
+              // Navigator.push(context, MaterialPageRoute(builder: (context) {
+              //   return Text('Detail page');
+              // },),
+              // );
             },
-            child: buildDayCard(Day.week[index]),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                buildDayCard(Day.week[index]),
+                dayListState [index] == 1 ? Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                    Row(
+                      children: [
+                        Text("Завтрак: "),
+                        Text("Какая-то вкусняшка)"),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text("Обед: "),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text("Ужин: "),
+                      ],
+                    )]),
+                ) :
+                Container(color: Colors.red),
+              ],
+            ),
           );
         },
       );
     }
-  }
+}
 Widget buildDayCard(Day day) {
   return Card(
-    elevation: 6.0,
+    elevation: 8.0,
     color: AppColors.mainLightBlue,
     shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.only(
@@ -33,13 +77,14 @@ Widget buildDayCard(Day day) {
             topRight: Radius.circular(10.0)
         )
     ),
-    child: Padding( padding: const EdgeInsets.all(16.0),
-      child: Column( children: <Widget>[ /*Text(day.id.toString()),
+    child: Padding( padding: const EdgeInsets.all(10.0),
+      child: Column( children: [ /*Text(day.id.toString()),
           const SizedBox( height: 14.0, ),*/
         Text( day.label,
           style: const TextStyle( fontSize: 20.0, fontWeight: FontWeight.w700,
             fontFamily: 'Palatino', ),
-        )
-      ], ), ),
+        ),
+        ], ),
+    ),
   );
 }
